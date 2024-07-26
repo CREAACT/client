1ee0f61e82c67a70104071ab5e2c164f96e33638
CREAACT <dzhabborovruslan8@gmail.com>
2024-07-26 22:10:10 +0300

    edit error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    sassOptions: {
+      fiber: false,
+    },
+  }
+  
+  module.exports = nextConfig
